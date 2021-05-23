b1f7b00b619270e2adb533dbcb4392c48102b7b5
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-05-22 21:03:27 -0400

    Created executable and the entry method

--- lib/hacker_weather.rb
+++ lib/hacker_weather.rb
@@ -4,5 +4,15 @@ require_relative "hacker_weather/version"
 
 module HackerWeather
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.get_weather
+    city = ARGV[0]
+
+    if city == nil
+      puts "The program requires city name."
+      exit
+    end
+
+    puts "Showing the weather for #{city}"
+  end
 end
